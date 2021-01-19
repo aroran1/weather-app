c0d36b90b5a193cbfd750d2045d693519c3493a9
aroran1 <nidhigulati2001@gmail.com>
2021-01-19 14:16:39 +0000

    weather-app: weather report printed

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 const request = require('request');
 
-const url = 'http://api.weatherstack.com/current?access_key=ff1dcc8b11a93789f32a23faa22a2f6d&query=london';
+const url = 'http://api.weatherstack.com/current?access_key=ff1dcc8b11a93789f32a23faa22a2f6d&query=london&units=f';
 
-request({ url: url}, (error, response) => {
-  console.log(response);
-  const data = JSON.parse(response.body);
-  console.log(data);
-})
+// queries
+// access_key=ff1dcc8b11a93789f32a23faa22a2f6d for api access
+// query=london for location
+// units=f for Fahrenheit or celcius is default
+
+// request({ url: url}, (error, response) => {
+//   console.log(response);
+//   const data = JSON.parse(response.body); // by passing json: true with request it handles response JSON.parse
+//   console.log(data);
+// })
+
+request({url: url, json: true}, (error, response) => {
+  // console.log(response.body.current);
+  const currentWeather = response.body.current;
+  console.log(`${currentWeather.weather_descriptions[0]} - It is currently ${currentWeather.temperature} degrees Fahrenheit out but it feels like ${currentWeather.feelslike} degrees.`);
+});
