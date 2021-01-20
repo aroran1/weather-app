5578d92a4478e4b636053c42c97e1c17120786e5
aroran1 <nidhigulati2001@gmail.com>
2021-01-20 00:15:35 +0000

    weather-app: code optimisation

--- app.js
+++ app.js
@@ -2,17 +2,17 @@ const forecast = require('./utils/forecast');
 const geocode = require('./utils/geocode');
 
 geocode('Boston', (error, geoResponse) => {
-  console.log('Error', error);
-  console.log('Geo Response', geoResponse);
-
+  if(error) {
+    return console.log('Error', error);
+  }
   const longitude = geoResponse.longitude;
   const latitude = geoResponse.latitude;
   const location = geoResponse.location;
 
-  if (geoResponse && latitude && longitude)
   forecast(latitude, longitude, (error, forcastResponse) => {
-    console.log('Error', error);
-    console.log('Forcast Response', forcastResponse);
+    if(error) {
+      return console.log('Error', error);
+    }
     console.log(`${forcastResponse.description} - It is currently ${forcastResponse.temperature} degrees Fahrenheit out but it feels like ${forcastResponse.feelslike} degrees in ${location}.`);
   })
 });
