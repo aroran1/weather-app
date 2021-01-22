c23021e4ee9f49e776d4effb8aefaa60368b10a6
aroran1 <nidhigulati2001@gmail.com>
2021-01-22 18:11:42 +0000

    weather-app: hooked up with process.argv

--- app.js
+++ app.js
@@ -2,53 +2,73 @@ const yargs = require('yargs');
 const forecast = require('./utils/forecast');
 const geocode = require('./utils/geocode');
 
-// geocode('Boston', (error, geoResponse) => {
-//   // console.log('geocode', city);
-//   if(error) {
-//     return console.log('Error', error);
-//   }
-//   const longitude = geoResponse.longitude;
-//   const latitude = geoResponse.latitude;
-//   const location = geoResponse.location;
+// process.argv way - is a method to grab the parsed value from the terminal
+const address = process.argv[2];
+console.log('address', address);
 
-//   forecast(latitude, longitude, (error, forcastResponse) => {
-//     if(error) {
-//       return console.log('Error', error);
-//     }
-//     console.log(`${forcastResponse.description} - It is currently ${forcastResponse.temperature} degrees Fahrenheit out but it feels like ${forcastResponse.feelslike} degrees in ${location}.`);
-//   })
-// });
+// run in terminal as
+// node app.js "london" 
+// output as
+// address [
+//  '/usr/local/Cellar/node/15.3.0/bin/node',
+//  '/Users/nidhiarora/Development/node/weather-app/app.js',
+//  'london'
+// ]
 
-yargs.command({
-  command: 'getWeather',
-  describe: 'get weather report',
-  builder: {
-    location: {
-      describe: 'pass location',
-      demandOption: true,
-      type: 'string'
+if(!address) {
+  console.log('Please provide the address!');
+} else {
+  geocode(address, (error, geoResponse) => {
+    if(error) {
+      return console.log('Error', error);
     }
-  },
-  handler(argv) {
-    geocode(argv.location, (error, geoResponse) => {
-      console.log('geocode', argv.location);
+    const longitude = geoResponse.longitude;
+    const latitude = geoResponse.latitude;
+    const location = geoResponse.location;
+  
+    forecast(latitude, longitude, (error, forcastResponse) => {
       if(error) {
         return console.log('Error', error);
       }
-      const longitude = geoResponse.longitude;
-      const latitude = geoResponse.latitude;
-      const location = geoResponse.location;
+      console.log(`${forcastResponse.description} - It is currently ${forcastResponse.temperature} degrees Fahrenheit out but it feels like ${forcastResponse.feelslike} degrees in ${location}.`);
+    })
+  });
+}
 
-      forecast(latitude, longitude, (error, forcastResponse) => {
-        if(error) {
-          return console.log('Error', error);
-        }
-        console.log(`${forcastResponse.description} - It is currently ${forcastResponse.temperature} degrees Fahrenheit out but it feels like ${forcastResponse.feelslike} degrees in ${location}.`);
-      })
-    });
-  }
-});
 
+// Yargs way
+// yargs.command({
+//   command: 'getWeather',
+//   describe: 'get weather report',
+//   builder: {
+//     location: {
+//       describe: 'pass location',
+//       demandOption: true,
+//       type: 'string'
+//     }
+//   },
+//   handler(argv) {
+//     geocode(argv.location, (error, geoResponse) => {
+//       console.log('geocode', argv.location);
+//       if(error) {
+//         return console.log('Error', error);
+//       }
+//       const longitude = geoResponse.longitude;
+//       const latitude = geoResponse.latitude;
+//       const location = geoResponse.location;
+
+//       forecast(latitude, longitude, (error, forcastResponse) => {
+//         if(error) {
+//           return console.log('Error', error);
+//         }
+//         console.log(`${forcastResponse.description} - It is currently ${forcastResponse.temperature} degrees Fahrenheit out but it feels like ${forcastResponse.feelslike} degrees in ${location}.`);
+//       })
+//     });
+//   }
+// });
+
+// process.argv is the command line way to grabbing the 3rd value in the array
+// yargs is the package that makes it easy
 // run in terminal as below
 // node app.js getWeather --location="london"
 // response
